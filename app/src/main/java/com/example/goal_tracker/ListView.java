8b6df62a30b769c3ecf7b437d451de1f51334ced
j0i1b <j0i1b@ugrad.cs.ubc.ca>
2019-01-26 18:16:40 -0800

    working list view

--- app/src/main/java/com/example/goal_tracker/ListView.java
+++ app/src/main/java/com/example/goal_tracker/ListView.java
@@ -2,6 +2,8 @@ package com.example.goal_tracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 
 public class ListView extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ListView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        String[] foods = {"a","b","c","d", "e"};
+        ListAdapter tut_adaptor = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+        android.widget.ListView tut_Adaptor_View = (android.widget.ListView) findViewById(R.id.tut_adaptor);
+        tut_Adaptor_View.setAdapter(tut_adaptor);
     }
 }
